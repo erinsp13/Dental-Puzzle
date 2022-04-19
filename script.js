ab680004df228a75e7dd6d819688a535ee49a2cc
Erin Patterson <100452295+erinsp13@users.noreply.github.com>
2022-04-19 12:42:29 -0300

    adjusting timer to countdown

--- script.js
+++ script.js
@@ -1,46 +1,97 @@
-//SIMPLE
-
 //TIMER
-window.onload = function () {
-    let seconds = 00;
-    let tens = 00;
-    let appendTens = document.getElementById("tens");
-    let appendSeconds = document.getElementById("seconds");
-    let buttonStart = document.getElementById("button-start");
-    let Interval;
-  
-  buttonStart.onclick = function (){
-  
-    clearInterval(Interval);
-    Interval = setInterval(startTimer, 10);
+class Timer {
+  constructor(root) {
+    root.innerHTML = Timer.getHTML();
+
+    this.el = {
+      minutes: root.querySelector(".timer__part--minutes"),
+      seconds: root.querySelector(".timer__part--seconds"),
+      control: root.querySelector(".timer__btn--control"),
+    };
+
+    this.interval = null;
+    this.remainingSeconds = 120;
+
+    this.el.control.addEventListener("click", () => {
+      if (this.interval === null) {
+        this.start();
+      } else {
+        this.stop();
+      }
+    });
+
+    this.el.reset.addEventListener("click", () => {
+    const inputMinutes = prompt("Enter number of minutes:");
+
+      if (inputMinutes < 60) {
+        this.stop();
+        this.remainingSeconds = inputMinutes * 60;
+        this.updateInterfaceTime();
+      }
+    });
   }
-  
-  
-  function startTimer () {
-    tens++;
-  
-    if(tens <= 9){
-        appendTens.innerHTML = "0" + tens;
-    }
-  
-    if(tens > 9){
-        appendTens.innerHTML = tens;
-    }
-  
-    if (tens > 99) {
-        console.log("seconds");
-        seconds++;
-        appendSeconds.innerHTML = "0" + seconds;
-        tens = "0";
-        appendTens.innerHTML = "0" + 0;
-    }
-    
-    if (seconds > 9){
-        appendSeconds.innerHTML = seconds;
+
+  updateInterfaceTime() {
+    const minutes = Math.floor(this.remainingSeconds / 60);
+    const seconds = this.remainingSeconds % 60;
+
+    this.el.minutes.textContent = minutes.toString().padStart(2, "0");
+    this.el.seconds.textContent = seconds.toString().padStart(2, "0");
+  }
+
+  updateInterfaceControls() {
+    if (this.interval === null) {
+      this.el.control.innerHTML = `<span class="material-icons">play_arrow</span>`;
+      this.el.control.classList.add("timer__btn--start");
+      this.el.control.classList.remove("timer__btn--stop");
+    } else {
+      this.el.control.innerHTML = `<span class="material-icons">pause</span>`;
+      this.el.control.classList.add("timer__btn--stop");
+      this.el.control.classList.remove("timer__btn--start");
     }
   }
-  
+
+  start() {
+    if (this.remainingSeconds === 0) return;
+
+    this.interval = setInterval(() => {
+      this.remainingSeconds--;
+      this.updateInterfaceTime();
+
+      if (this.remainingSeconds === 0) {
+        this.stop();
+      }
+    }, 1000);
+
+    this.updateInterfaceControls();
   }
+
+  stop() {
+    clearInterval(this.interval);
+
+    this.interval = null;
+
+    this.updateInterfaceControls();
+  }
+
+  static getHTML() {
+    return `
+			<span class="timer__part timer__part--minutes">00</span>
+			<span class="timer__part">:</span>
+			<span class="timer__part timer__part--seconds">00</span>
+			<button type="button" class="timer__btn timer__btn--control timer__btn--start">
+				<span class="material-icons">play_arrow</span>
+        <button id="button-shuffle" onClick="shuffle();">Shuffle</button>
+			</button>
+
+		`;
+  }
+}
+
+new Timer(
+	document.querySelector(".timer")
+);
+
   //PUZZLE FUNCTION
   function swapTiles(cell1,cell2) {
   let temp = document.getElementById(cell1).className;
